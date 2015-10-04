9b0d5d1e9c4e512850dfb8b38263ebc5b2d2e51c
Kristian <a30068320@mcla.edu>
2015-10-04 16:44:53 -0400

    commiting

--- Assignment2/Assignment2.sh
+++ Assignment2/Assignment2.sh
@@ -1,12 +1,14 @@
 # comment out nmap code for development
-# sudo nmap -sP 192.168.0.0/24
+#sudo nmap -sP 192.168.0.0/24
 
 # call script to simulate nmap
 
-./nmap_simulate.sh
+./nmap_simulate.sh | grep -Eo "‘[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}’" | grep -Eo "Host" >> ip.txt
 
 
-grep -Eo Host
+#for ip in 192.168.1.{1..10}; do
+#ping -c 1 -t 1 $ip > /dev/null && 
+#echo "${ip} is up" >> Ips.txt ; done 
 
 #for i {1..10}
 #do
