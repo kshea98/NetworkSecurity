13309002faf55bd743e3de736f571335d22c7214
Kristian <a30068320@mcla.edu>
2015-10-13 11:25:44 -0400

    final

--- Assignment2/Assignment2.sh
+++ Assignment2/Assignment2.sh
@@ -1,17 +1,20 @@
 # comment out nmap code for development
-#sudo nmap -sP 192.168.0.0/24 | grep -io "[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}" >> mac.txt
+
+for i in {1..10}
+do
+sudo nmap -sP 192.168.0.0/24 | grep -io "[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}" >> mac.txt
 
 # call script to simulate nmap
 
-./nmap_simulate.sh | grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" >> ip.txt
+#./nmap_simulate.sh | grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" >> ip.txt
 
-./nmap_simulate.sh | grep -io "[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}" >> mac.txt
+#./nmap_simulate.sh | grep -io "[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}" >> mac.txt
 
-cat mac.txt | sort | uniq -c >> count.txt
+cat mac.txt | sort | uniq -c > count.txt
 
 echo This is the Hacker 
 cat count.txt | tail -1 
-
+done
 
 #for i {1..10}
 #do
